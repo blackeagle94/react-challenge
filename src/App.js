4c9e63232a76a642e5d0975ea1f88dd02c4f99e7
blackeagle94 <blackeagle4894@gmail.com>
2021-01-20 00:16:01 +0200

    json server installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import { useState} from 'react'
+import AddTask from './components/AddTask'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+
+    const [ showAddTask, setShowAddTask] = useState(false)
+
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctor Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Meeting at School',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Food Shoopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false
+        }
+    ])
+
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    const toggleReminder =(id) => {
+        setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
+    }
+
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 1000) + 1
+        const newTask = {
+            id: id, ...task
+        }
+        setTasks([...tasks, newTask])
+    }
+
     return ( <div className="container">
-            <Header />
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask}/>
+            {showAddTask && <AddTask addTask={addTask}/>}
+            {tasks.length > 0 ? <Tasks tasks={tasks} deleteTask={deleteTask} toggleReminder={toggleReminder}/> : 'No Tasks To Show'}
     </div> 
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Button from './Button'
 
-function Header({title}) {
+function Header({title, onAdd, showAddTask}) {
+
+   
+
     return (
         <header className="header">
             <h1>{title}</h1>
-            <button className="btn">Add</button>
+            <Button color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
